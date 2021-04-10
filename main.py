5ae21337732db4d0ad8d70da8dda2ad2c01afc7c
subarubknb03 <subaru519kqka@gmail.com>
2021-04-10 09:06:17 +0900

    update main.py myfunc.py v1.0

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ print('整理したいフォルダのパスを入力してください')
 path = input()
 
 path = f.unify_path_name(path)
-file_extension = f.check_file_extension(path)
-f.make_dir(file_extension, path)
+file_extension = f.check_file_extensions(path)
+f.make_directories(path)
 f.move_f(file_extension, path)
