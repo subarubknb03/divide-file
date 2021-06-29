9f3099f67ef31bed17ecf5d33b76e23810d427c8
subarubknb03 <subaru519kqka@gmail.com>
2021-06-29 15:07:02 +0900

    update main.py README .gitignore

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import myfunc as f
 
 # 整理したいフォルダのパスを入力
-print('整理したいフォルダのパスを入力してください')
-path = input()
+if __name__ == '__main__':
+    print('整理したいフォルダのパスを入力してください')
+    path = input()
 
-path = f.unify_path_name(path)
-file_extension = f.check_file_extensions(path)
-f.make_directories(path)
-f.move_f(file_extension, path)
+    path = f.unify_path_name(path)
+    file_extension = f.check_file_extensions(path)
+    f.make_directories(path)
+    f.move_f(file_extension, path)
